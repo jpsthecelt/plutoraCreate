c6b74499685244cfd65c9f818c1f64925ee1850e
jpsthecelt <jpsthecacelt@netscape.net>
2016-12-25 08:20:09 -0800

    Added some try/except logic, as well as more comments.

--- plSystemCreate.py
+++ plSystemCreate.py
@@ -15,6 +15,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 # parse commandline and get appropriate passwords
 #    accepted format is python plSuystemCreate.py -f <config fiiename> -pusername:password
+#
 parser = argparse.ArgumentParser(description='Get user/password Plutora and configuration-filename.')
 #   help='JIRA and Plutora logins (username:password)')
 parser.add_argument('-f', action='store', dest='cfgFilename',
@@ -27,22 +28,20 @@ cfgFilename = results.cfgFilename.split(':')[0]
 PlutoraUsername = results.pltUnP.split(':')[0].replace('@', '%40')
 PlutoraPassword = results.pltUnP.split(':')[1]
 
+# If we don't specify a configfile on the commandline, assume one & try accessing
 if len(cfgFilename) <= 0:
     cfgFilename = 'syscreate.cfg'
 
-# ClientId & Secret from manual setup of Plutora Oauth authorization.
-#    read from configFile
+# using the specified/assumed configfilename, grab ClientId & Secret from manual setup of Plutora Oauth authorization.
 try:
     with open(cfgFilename) as data_file:
         data = json.load(data_file)
     clientid = data["credentials"]["clientId"]
     clientsecret = data["credentials"]["clientSecret"]
 except:
-    exit('couldnt open file {0}',cfgFilename)
+    exit('couldnt open file {0}'.format(cfgFilename))
 
-
-
-# Setup for Get authorization-token
+# Setup to obtain Get authorization-token
 authTokenUrl = "https://usoauth.plutora.com/oauth/token"
 payload = 'client_id=' + clientid + '&client_secret=' + clientsecret + '&' + 'grant_type=password&username='
 payload = payload + PlutoraUsername + '&password=' + PlutoraPassword + '&='
@@ -75,7 +74,7 @@ headers = {
     'postman-token': "bc355474-15d1-1f56-6e35-371b930eac6f"
 }
 
-# Get Plutora information for all system releases
+# Get Plutora information for all system releases, or systems, or just the organization-tree
 getReleases = '/releases/9d18a2dc-b694-4b20-971f-4944420f4038'
 getSystems = '/systems'
 getOrganizationsTree = '/organizations/tree'
@@ -89,32 +88,38 @@ else:
     print('\npltSystemCreate.py - Plutora get of organizations information:')
     pp.pprint(r.json())
 
-fstChildsId = r.json()['childs'][0]['id']
+# OK; try creating a new system...
+try:
+    fstChildsId = r.json()['childs'][0]['id']
+    payload = r"""{
+      "Name": "API created System 1",
+      "Vendor": "API created vendor",
+      "Status": "Active",
+      "OrganizationId": "%s",
+      "Description": "Description of API created System 1"
+    }""" % r.json()['childs'][0]['id']
 #"additionalInformation":[],
-payload = r"""{
-  "Name": "API created System 1",
-  "Vendor": "API created vendor",
-  "Status": "Active",
-  "OrganizationId": "%s",
-  "Description": "Description of API created System 1"
-}""" % r.json()['childs'][0]['id']
-
-postSystem = '/systems'
-print("Here's what I'm sending Plutora (headers & payload):")
-print("header: ",headers)
-print("payload: ",payload)
-
-r = requests.post(plutoraBaseUrl+postSystem, data=payload, headers=headers)
-if r.status_code != 200:
-    print r.status_code
-    print('\npltSystemCreate.py: too bad sucka! - [failed on Plutora create system POST]')
-    pp.pprint(r.json())
-    exit('Sorry, unrecoverable error; gotta go...')
-else:
-    print('\npltSystemCreate.py - Plutora POST of new system information:')
-    pp.pprint(r.json())
-
+    
+    postSystem = '/systems'
+    print("Here's what I'm sending Plutora (headers & payload):")
+    print("header: ",headers)
+    print("payload: ",payload)
+    
+    r = requests.post(plutoraBaseUrl+postSystem, data=payload, headers=headers)
+    if r.status_code != 200:
+        print r.status_code
+        print('\npltSystemCreate.py: too bad sucka! - [failed on Plutora create system POST]')
+        pp.pprint(r.json())
+        exit('Sorry, unrecoverable error; gotta go...')
+    else:
+        print('\npltSystemCreate.py - Plutora POST of new system information:')
+        pp.pprint(r.json())
+except:
+    exit('Error during API processing [POST]')
+    
 print("\n\nWell, it seems we're all done here, boys")
+
+# Residual stuff follows:
 #for i in r.json()["issues"]:
 #    print("field is", i["fields"]["description"])
 #    r = requests.post(plutoraMaerskUrl, data=i["fields"]["description"], headers=headers)
